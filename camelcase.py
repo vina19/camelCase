735d3acf2961a8588243d4e0ee7a7555791ba3ae
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-09-05 18:45:38 -0500

    Created camelcase.py

---
+++ camelcase.py
@@ -0,0 +1,13 @@
+#inputing the sentence from the user
+sentence = input("Please enter a sentence: ")
+
+#convert the first letter to uppercase from the sentence
+upper_words = sentence.title()
+
+#not adding space in the result sentence
+no_spaces = upper_words.replace(' ', '')
+joined = ''.join(no_spaces)
+camelcase = joined[0].lower() + joined[1:]
+
+#Display the result
+print(camelcase)
